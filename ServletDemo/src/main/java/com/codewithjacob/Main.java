bef31fa20167ab9f52486dd80a136fd314bc47c2
shizhiang <shizhiang@126.com>
2023-03-15 08:27:48 +0800

    Run servlet by embedded tomcat

--- ServletDemo/src/main/java/com/codewithjacob/Main.java
+++ ServletDemo/src/main/java/com/codewithjacob/Main.java
@@ -6,8 +6,37 @@ package com.codewithjacob;/*
  *@create $DATE $TIME
  */
 
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LifecycleException {
         System.out.println("Hello Java Servlet!");
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+
+        Context ctx = tomcat.addContext("", null);
+
+        Tomcat.addServlet(ctx, "hello", new HelloServlet());
+        ctx.addServletMappingDecoded("/hello", "hello");
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+
+    public static class HelloServlet extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            resp.getWriter().println("hello world!");
+        }
     }
 }
